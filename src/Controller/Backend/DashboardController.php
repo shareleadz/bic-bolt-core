f4d0588d3e5ff6aebea591485f304d5c73ef22ce
nourdinateur <nafzaouinourdine@gmail.com>
2022-10-04 17:59:47 +0100

    added distributors dashboard template for country managers

--- src/Controller/Backend/DashboardController.php
+++ src/Controller/Backend/DashboardController.php
@@ -8,34 +8,46 @@ use Bolt\Controller\TwigAwareController;
 use Bolt\Storage\Query;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class DashboardController extends TwigAwareController implements BackendZoneInterface
 {
     /**
      * @Route("/", name="bolt_dashboard", methods={"GET"})
      */
-    public function index(Query $query): Response
+    public function index(Query $query, Security $security): Response
     {
-        $this->denyAccessUnlessGranted('dashboard');
+        if($security->getUser()->getRoles()[0] == 'ROLE_COUNTRY_MANAGER'){
+            $pager = $this->createPager($query, "distributors", 1000, '-modifiedAt');
+            $page = (int) $this->request->get('page', 1);
+            $records = $pager->setCurrentPage($page);
+            $filter = $this->getFromRequest('filter');
+            return $this->render('@bolt/pages/distributors.twig', [
+                'records' => $records,
+                'filter_value' => $filter,
+            ]);
+        }else{
+            $this->denyAccessUnlessGranted('dashboard');
 
-        // TODO PERMISSIONS: implement listing that only lists content that the user is allowed to see
-        $amount = (int) $this->config->get('general/records_per_page', 10);
-        $page = (int) $this->request->get('page', 1);
-        $contentTypes = $this->config->get('contenttypes')->where('show_on_dashboard', true)->keys()->implode(',');
-        $filter = $this->getFromRequest('filter');
+            // TODO PERMISSIONS: implement listing that only lists content that the user is allowed to see
+            $amount = (int) $this->config->get('general/records_per_page', 10);
+            $page = (int) $this->request->get('page', 1);
+            $contentTypes = $this->config->get('contenttypes')->where('show_on_dashboard', true)->keys()->implode(',');
+            $filter = $this->getFromRequest('filter');
 
-        $pager = $this->createPager($query, $contentTypes, $amount, '-modifiedAt');
-        $nbPages = $pager->getNbPages();
+            $pager = $this->createPager($query, $contentTypes, $amount, '-modifiedAt');
+            $nbPages = $pager->getNbPages();
 
-        if ($page > $nbPages) {
-            return $this->redirectToRoute('bolt_dashboard');
+            if ($page > $nbPages) {
+                return $this->redirectToRoute('bolt_dashboard');
+            }
+
+            $records = $pager->setCurrentPage($page);
+
+            return $this->render('@bolt/pages/dashboard.html.twig', [
+                'records' => $records,
+                'filter_value' => $filter,
+            ]);
         }
-
-        $records = $pager->setCurrentPage($page);
-
-        return $this->render('@bolt/pages/dashboard.html.twig', [
-            'records' => $records,
-            'filter_value' => $filter,
-        ]);
     }
 }
